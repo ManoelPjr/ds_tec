d5eb1a41950df327becc8568c4ba829d66ba4d9a
ManoelPjr <napolesjr@gmail.com>
2021-10-13 13:02:56 -0300

    Added new Controllers
    added taxaJuros - calculaJuros - showmethecode

---
+++ dstec/Controllers/calculaJurosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Globalization;
+
+namespace dstec.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class calculajurosController : Controller
+    {
+        // GET: calculaJurosController
+        [HttpGet]
+        public string calculaJuros(double valorInicial,int tempo)
+        {
+            double taxa = 0.01;
+            double temp = valorInicial * (1 + taxa);
+            double valorFinal = (Math.Pow(temp, tempo))/100;
+            valorFinal = Math.Truncate(valorFinal) / 100;
+            return String.Format(CultureInfo.InvariantCulture, "{0:0.00}", valorFinal);
+        }
+    }
+}
---
+++ dstec/Controllers/showmethecodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dstec.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class showmethecodeController : Controller
+    {
+        // GET: showmethecodeController
+        [HttpGet]
+        public string showmethecode()
+        {
+            String urlGit = "https://github.com/ManoelPjr/ds_tec";
+            return urlGit;
+        }
+    }
+}
